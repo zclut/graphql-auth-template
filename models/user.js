13b235659064553d37ab442d0e92e797c4133f16
zClut <juandanielpadilla0@gmail.com>
2022-05-29 02:03:23 +0100

    :100: | Creating user schema

---
+++ models/user.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.model('User', schema)
